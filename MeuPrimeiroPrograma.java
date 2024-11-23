33e64e49fd2cca27c62cf660ce7ed53684967987
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2024-11-23 16:49:36 -0300

    more archives

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,13 @@
+public class MeuPrimeiroPrograma {
+  public static void main(String entrada[]) {
+    int inteiro = 20;
+    char caracter = 'F';
+    double real = 1.85;
+    String frase = "What's up?";
+    boolean VF = true;
+    if(VF == true) {
+      System.out.println("Hi there, " + frase + " I'm " + inteiro + " years old and I'm " + real+ "m tall");
+    }
+    System.exit(0);
+  }
+}
